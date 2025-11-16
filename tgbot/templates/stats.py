86f037f73f577857fcb578a9b8e6d0f167e9d213
pashagta555 <83071677+pashagta555@users.noreply.github.com>
2025-11-16 03:22:42 +0300

    Update stats.py
    It was fully translated to English

--- tgbot/templates/stats.py
+++ tgbot/templates/stats.py
@@ -9,16 +9,16 @@ from .. import callback_datas as calls
 def stats_text():
     stats = get_stats()
     txt = textwrap.dedent(f"""
-        📊 <b>Статистика Playerok бота</b>
+        📊 <b>Statistics of the  Playerok bot</b>
 
-        Дата запуска бота: <b>{stats.bot_launch_time.strftime("%d.%m.%Y %H:%M:%S") or 'Не запущен'}</b>
+       Date of the start of the bot: <b>{stats.bot_launch_time.strftime("%d.%m.%Y %H:%M:%S") or 'Не запущен'}</b>
 
-        <b>Статистика с момента запуска:</b>
-        ┣ Выполнено: <b>{stats.deals_completed}</b>
-        ┣ Возвратов: <b>{stats.deals_refunded}</b>
-        ┗ Заработано: <b>{stats.earned_money}</b>₽
+        <b>Statistics from the start:</b>
+        ┣ Finished: <b>{stats.deals_completed}</b>
+        ┣ Refunded: <b>{stats.deals_refunded}</b>
+        ┗ Earned: <b>{stats.earned_money}</b>₽
 
-        Выберите действие ↓
+        Select action ↓
     """)
     return txt
 
@@ -26,8 +26,8 @@ def stats_text():
 def stats_kb():
     rows = [
         [
-        InlineKeyboardButton(text="⬅️ Назад", callback_data=calls.MenuNavigation(to="default").pack()),
-        InlineKeyboardButton(text="🔄️ Обновить", callback_data=calls.MenuNavigation(to="stats").pack())
+        InlineKeyboardButton(text="⬅️ Back", callback_data=calls.MenuNavigation(to="default").pack()),
+        InlineKeyboardButton(text="🔄️ Update", callback_data=calls.MenuNavigation(to="stats").pack())
         ]
     ]
     kb = InlineKeyboardMarkup(inline_keyboard=rows)
