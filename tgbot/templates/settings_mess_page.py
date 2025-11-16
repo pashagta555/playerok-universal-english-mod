5e79ab012146945d936fea389fdd42d904e623b4
pashagta555 <83071677+pashagta555@users.noreply.github.com>
2025-11-16 03:37:07 +0300

    Update settings_mess_page.py
    It was fully translated to en

--- tgbot/templates/settings_mess_page.py
+++ tgbot/templates/settings_mess_page.py
@@ -8,30 +8,30 @@ from .. import callback_datas as calls
 
 def settings_mess_page_text(message_id: int):
     messages = sett.get("messages")
-    enabled = "🟢 Включено" if messages[message_id]["enabled"] else "🔴 Выключено"
-    message_text = "\n".join(messages[message_id]["text"]) or "❌ Не задано"
+    enabled = "🟢 Turned on" if messages[message_id]["enabled"] else "🔴 Turned off"
+    message_text = "\n".join(messages[message_id]["text"]) or "❌ Not specified"
     txt = textwrap.dedent(f"""
-        ✒️ <b>Редактирование сообщения</b>
+        ✒️ <b>Edit message</b>
 
-        🆔 <b>ID сообщения:</b> {message_id}
-        💡 <b>Состояние:</b> {enabled}
-        💬 <b>Текст сообщения:</b> <blockquote>{message_text}</blockquote>
+        🆔 <b>ID of the message:</b> {message_id}
+        💡 <b>Status:</b> {enabled}
+        💬 <b>Message text:</b> <blockquote>{message_text}</blockquote>
 
-        Выберите параметр для изменения ↓
+        Select parametre to be changed ↓
     """)
     return txt
 
 
 def settings_mess_page_kb(message_id: int, page: int = 0):
     messages = sett.get("messages")
-    enabled = "🟢 Включено" if messages[message_id]["enabled"] else "🔴 Выключено"
-    message_text = "\n".join(messages[message_id]["text"]) or "❌ Не задано"
+    enabled = "🟢 Turned on" if messages[message_id]["enabled"] else "🔴 Turned off"
+    message_text = "\n".join(messages[message_id]["text"]) or "❌ Not specified"
     rows = [
-        [InlineKeyboardButton(text=f"💡 Состояние: {enabled}", callback_data="switch_message_enabled")],
-        [InlineKeyboardButton(text=f"💬 Текст сообщения: {message_text}", callback_data="enter_message_text")],
+        [InlineKeyboardButton(text=f"💡 Status: {enabled}", callback_data="switch_message_enabled")],
+        [InlineKeyboardButton(text=f"💬 Message text: {message_text}", callback_data="enter_message_text")],
         [
-        InlineKeyboardButton(text="⬅️ Назад", callback_data=calls.MessagesPagination(page=page).pack()),
-        InlineKeyboardButton(text="🔄️ Обновить", callback_data=calls.MessagePage(message_id=message_id).pack())
+        InlineKeyboardButton(text="⬅️ Back", callback_data=calls.MessagesPagination(page=page).pack()),
+        InlineKeyboardButton(text="🔄️ Update", callback_data=calls.MessagePage(message_id=message_id).pack())
         ]
     ]
     kb = InlineKeyboardMarkup(inline_keyboard=rows)
@@ -40,7 +40,7 @@ def settings_mess_page_kb(message_id: int, page: int = 0):
 
 def settings_mess_page_float_text(placeholder: str):
     txt = textwrap.dedent(f"""
-        ✒️ <b>Редактирование сообщения</b>
+        ✒️ <b>Edit message</b>
         \n{placeholder}
     """)
     return txt
